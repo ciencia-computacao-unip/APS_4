118c79daab86568ffbe7d01cafda236e3a9f5494
Victor Perin <me@victorperin.com>
2015-10-13 00:50:29 -0300

    inserindo tabelas scores e usuários no banco de dados

--- database.sql
+++ database.sql
@@ -9,7 +9,35 @@
 /*!40101 SET NAMES utf8mb4 */;
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+
+-- Copiando estrutura do banco de dados para genius_java
+CREATE DATABASE IF NOT EXISTS `genius_java` /*!40100 DEFAULT CHARACTER SET latin1 */;
+USE `genius_java`;
+
+
+-- Copiando estrutura para tabela genius_java.scores
+CREATE TABLE IF NOT EXISTS `scores` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `id_usuario` int(10) unsigned NOT NULL,
+  `score` bigint(20) unsigned NOT NULL DEFAULT '0',
+  `modo_de_jogo` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `score` (`score`),
+  KEY `id_usuario` (`id_usuario`),
+  KEY `modo_de_jogo` (`modo_de_jogo`),
+  CONSTRAINT `FK_scores_usuarios` FOREIGN KEY (`id_usuario`) REFERENCES `usuarios` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
 -- Exportação de dados foi desmarcado.
+
+
+-- Copiando estrutura para tabela genius_java.usuarios
+CREATE TABLE IF NOT EXISTS `usuarios` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `nome` varchar(20) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `nome` (`nome`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- Exportação de dados foi desmarcado.
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
