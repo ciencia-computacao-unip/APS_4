2061bccd1e6f3f7477827de6eccdf4049743a277
DEGRECCI <rickdegrecci@hotmail.com>
2015-11-10 09:33:23 -0200

    Classe botao foi alterada para Botao, e nao foram alterados nestes arquivos

--- Jogo.java
+++ Jogo.java
@@ -14,27 +14,27 @@ public class Jogo extends JFrame implements ActionListener {
 
 	// DECLARANDO VARIAVEIS;
 	private static final long serialVersionUID = 1L;
-	private JPanel jp_total,jp_center,jp_east,jp_north,jp_south,jp_west;	
-	private botao[] Botao = new botao[20];
+	private JPanel jp_total,jp_center,jp_east,jp_north,jp_south,jp_west;
+	private Botao[] Botao = new Botao[20];
 	public int daltonico;
-	
-	
+
+
 	public JFrame frame = new JFrame("Genius");
-	
-	
+
+
 	//MTD. CONSTRUTOR;
 	public Jogo(){
-		
+
 		super("Jogo");
-		
+
 		//Setando configs da janela
-		frame.setDefaultCloseOperation(EXIT_ON_CLOSE); 
+		frame.setDefaultCloseOperation(EXIT_ON_CLOSE);
 		frame.setLocation(0,0);
 		frame.setSize(200,180);
-		frame.setResizable(false); 
-		frame.setVisible(true);		
+		frame.setResizable(false);
+		frame.setVisible(true);
 		frame.setExtendedState(JFrame.MAXIMIZED_BOTH);
-		
+
 		//Layouts
 		jp_total = new JPanel(new BorderLayout());
 		jp_center = new JPanel(new GridLayout(2,2));
@@ -42,9 +42,9 @@ public class Jogo extends JFrame implements ActionListener {
 		jp_south = new JPanel(new FlowLayout());
 		jp_east = new JPanel(new FlowLayout());
 		jp_west = new JPanel(new FlowLayout());
-		
+
 		//BOTOES DO JOGO
-	
+
 		Color[] Collor = new Color[10];
 		Collor[1] = Color.BLUE;
 		Collor[2] = Color.RED;
@@ -54,100 +54,100 @@ public class Jogo extends JFrame implements ActionListener {
 		Collor[6] = Color.CYAN;
 		Collor[7] = Color.MAGENTA;
 		Collor[8] = Color.WHITE;
-		
-		
+
+
 		//JOGO
-			
-		for(int x=1;x<5;x++){ 
-			Botao[x]= new botao();
+
+		for(int x=1;x<5;x++){
+			Botao[x]= new Botao();
 			jp_center.add(Botao[x]);
 			Botao[x].addActionListener(this);
-			
-		}	
-		
+
+		}
+
 		//MENU
 		for(int x=5;x<7;x++){ 
-		
-			Botao[x]= new botao();
+
+			Botao[x]= new Botao();
 			Botao[x].addActionListener(this);
 			jp_south.add(Botao[x]);
 			Botao[x].setPreferredSize(new Dimension(200, 80));
 			Botao[x].setBorder(new LineBorder(Color.WHITE, 3));
 			Botao[x].setForeground(Color.WHITE);
-			
+
 		}
-		
+
 		Botao[5].setText("JOGAR");
 		Botao[6].setText("VOLTAR");
-		
+
 		if(daltonico==0){
-			
+
 			jp_north.setBackground(Color.BLACK);
 			jp_south.setBackground(Color.BLACK);
 			jp_east.setBackground(Color.BLACK);
 			jp_west.setBackground(Color.BLACK);
-			
-			for(int x=1;x<5;x++){ 
-				
+
+			for(int x=1;x<5;x++){
+
 				Color Col = Collor[x];
 				Botao[x].setBorder(new LineBorder(Col, 10));
 				Botao[x].setBackground(Col);
-				
+
 			}
-			for(int x=5;x<7;x++){ 
-			
+			for(int x=5;x<7;x++){
+
 				Color Col = Collor[x];
-				Botao[x].setBackground(Color.BLACK);			
-			
+				Botao[x].setBackground(Color.BLACK);
+
 			}
-			
+
 		}else{
-			
+
 			jp_north.setBackground(Color.GRAY);
 			jp_south.setBackground(Color.GRAY);
 			jp_east.setBackground(Color.GRAY);
 			jp_west.setBackground(Color.GRAY);
 
-			for(int x=1;x<5;x++){ 
-				
+			for(int x=1;x<5;x++){
+
 				Color Col = Collor[x+4];
 				Botao[x].setBorder(new LineBorder(Col, 10));
 				Botao[x].setBackground(Col);
-				
+
 			}
-			for(int x=5;x<7;x++){ 
-			
+			for(int x=5;x<7;x++){
+
 				Color Col = Collor[x];
-				Botao[x].setBackground(Color.GRAY);				
-			
+				Botao[x].setBackground(Color.GRAY);
+
 			}
-			
+
 		}
-		
-		
-	
+
+
+
 		//ADDS
-		
+
 		frame.add(jp_total);
 		jp_total.add(jp_center,BorderLayout.CENTER);
 		jp_total.add(jp_east,BorderLayout.EAST);
 		jp_total.add(jp_west,BorderLayout.WEST);
 		jp_total.add(jp_north,BorderLayout.NORTH);
 		jp_total.add(jp_south,BorderLayout.SOUTH);
-		
 
-		
+
+
 	}
-	
+
 	public void actionPerformed(ActionEvent e){
 		if (e.getSource()== Botao[6]){
 			frame.dispose();
 		}
 	}
-	
+
 	public static void main(String[] args) {
 
 	new Jogo();
-	
+
 	}
 }
--- MenuDificuldade.java
+++ MenuDificuldade.java
@@ -14,53 +14,53 @@ public class MenuDificuldade extends JFrame implements ActionListener {
 	// DECLARANDO VARIAVEIS;
 	private static final long serialVersionUID = 1L;
 	private JPanel jp_total;
-	private botao[] Botao = new botao[5];
-	
-	
+	private Botao[] Botao = new Botao[5];
+
+
 	public JFrame frame = new JFrame("Dificuldade");
-	
-	
-	
+
+
+
 	//MTD. CONSTRUTOR;
 	public MenuDificuldade(){
-		
+
 		super("Tela Menu");
-		
-		
+
+
 		//Setando configs da janela
-		frame.setDefaultCloseOperation(DISPOSE_ON_CLOSE); 
+		frame.setDefaultCloseOperation(DISPOSE_ON_CLOSE);
 		frame.setLocation(150,150);
 		frame.setSize(150,200);
-		frame.setResizable(true); 
+		frame.setResizable(true);
 		frame.setVisible(true);
-		
+
 		//Layouts
-		
+
 		jp_total = new JPanel(new GridLayout(4,1,2,2));
 		for(int x=1;x<5;x++){ 
-			Botao[x]= new botao();
+			Botao[x]= new Botao();
 			Botao[x].setBorder(new LineBorder(Color.BLACK, 1));
 			jp_total.add(Botao[x]);
 			Botao[x].addActionListener(this);
 		}
 		frame.add(jp_total);
-		
+
 		Botao[1].setText("8 SEQUENCIAS");
 		Botao[2].setText("14 SEQUENCIAS");
 		Botao[3].setText("20 SEQUENCIAS");
 		Botao[4].setText("31 SEQUENCIAS");
-	
-		
-		
-		
+
+
+
+
 	}
-	
-	
+
+
 	public void actionPerformed(ActionEvent e){
-	
+
 		if (e.getSource()== Botao[1]){
-			new Jogo();	
-			frame.dispose();			
+			new Jogo();
+			frame.dispose();
 		}if (e.getSource()== Botao[2]){
 			new Jogo();
 			frame.dispose();
@@ -72,4 +72,4 @@ public class MenuDificuldade extends JFrame implements ActionListener {
 			frame.dispose();
 		}
 	}
-}	
+}
