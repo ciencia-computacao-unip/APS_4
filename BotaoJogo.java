9a34f71cd7bc1cb046e103cf9686ee323faede92
Victor Perin <me@victorperin.com>
2015-11-11 08:35:06 -0200

    Refactor(Botão Jogo): Removendo tamanho do botão por enquanto

--- BotaoJogo.java
+++ BotaoJogo.java
@@ -1,12 +1,9 @@
 import javax.swing.JButton;
-import java.awt.Dimension;
-
 
 public class BotaoJogo extends JButton{
 
   public BotaoJogo(){
     super();
-    this.setPreferredSize( new Dimension(200, 80) );
   }
 
 }
